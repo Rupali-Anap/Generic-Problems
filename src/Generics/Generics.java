fd4d775152e4bc571e2f2a60c12b6cc43b1058a8
Rupali Anap <anaprupali333@gmail.com>
2022-11-23 04:39:36 -0800

    Added: Float Number

--- src/Generics/Generics.java
+++ src/Generics/Generics.java
@@ -2,8 +2,8 @@ package Generics;
 
 public class Generics {
 
-    public int maximum(Integer x, Integer y, Integer z) {
-        Integer max = x;
+    public Float maximum(Float x, Float y, Float z) {
+        Float max = x;
         if (y.compareTo(max) > 0 && y.compareTo(z) > 0)
             max = y;
         else if (z.compareTo(max) > 0)
--- src/Generics/GenericsFloat.java
+++ src/Generics/GenericsFloat.java
@@ -1,20 +1,19 @@
 package Generics;
 import java.util.Scanner;
 
-public class GenericsMaximum {
-    public static void main(String[] args) {
+public class GenericsFloat {
 
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Finding the Maximum number from given numbers:");
         System.out.println("Enter 3 numbers: ");
-        Integer x = sc.nextInt();
-        Integer y = sc.nextInt();
-        Integer z = sc.nextInt();
+        float x = sc.nextFloat();
+        float y = sc.nextFloat();
+        float z = sc.nextFloat();
         Generics findMax=new Generics();
-        System.out.println("Maxmum Number: " + findMax.maximum(x, y, z));
+        System.out.println("float Number: " + findMax.Generics (x, y, z));
 
     }
 
 }
-
