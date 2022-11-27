67e3aa16cd006fed8fac47a64b8096d13be04008
Rupali Anap <anaprupali333@gmail.com>
2022-11-27 01:36:50 -0800

    Added: Maximum string

---
+++ src/Generics/MaxString.java
@@ -0,0 +1,21 @@
+package Generics;
+import java.util.Scanner;
+
+public class MaxString {
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Finding the Maximum String from given Strings:");
+            System.out.println("Enter the Three Strings: ");
+            String x = sc.next();
+            String y = sc.next();
+            String z = sc.next();
+            Generics findMax=new Generics();
+            System.out.println("Maximum Number: " + findMax.maximum(x, y, z));
+
+        }
+
+    }
+
